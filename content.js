55723b88a16aedc7caee41dc987898f0f69cbefd
Mayank Gaur <mayankgaur13004@gmail.com>
2026-09-23 17:02:57 +0530

    refactor: regroup assets, pages, and scripts into dedicated subdirectories

--- content.js
+++ content.js
@@ -51,7 +51,7 @@ function checkAndRedirect() {
 
         if (enabled && shouldBlock) {
             blockTimeout = setTimeout(() => {
-                window.location.href = chrome.runtime.getURL("trying1.html");
+                window.location.href = chrome.runtime.getURL("pages/trying1.html");
             }, delay);
         }
     });
